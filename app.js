23c66a86c7c64529da9a2865e92d7c470c9cb968
gary909 <garywhite61@hotmail.com>
2023-04-20 13:16:42 +0200

    Forfeited this one :-(

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 function isOpposite(s1,s2){
   //coding here...
+//   let myStr = "";
+//   for(let i = s1.length - 1; i >=0; i--){
+//     myStr = myStr + s1[i];
+//   }
+//   return myStr
+
+// let myStr = "";
+// for(let i = s1.length - 1; i >=0; i--){
+//     if(s1[i] == s1[i].toUpperCase()){
+//         myStr = myStr + s1[i].toLowerCase();
+//     } else if (s1[i] == s1[i].toLowerCase()){
+//         myStr = myStr + s1[i].toUpperCase();
+//     }
+// }
+// return myStr
+
+// return reverseString(s1)
+
+// return s1.length;
+
+
+function isOpposite(s1, s2) {
+    if (s1.length !== s2.length || (s1 === '' && s2 === '')) {
+      return false;
+    }
   
+    for (let i = 0; i < s1.length; i++) {
+      if (s1.charAt(i) === s2.charAt(i) || s1.charAt(i).toLowerCase() !== s2.charAt(i).toLowerCase()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+//Forfeited this one :-(
+
+
+
+
 }
 
 console.log(isOpposite("ab","AB")); // true
