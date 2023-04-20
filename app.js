d7da20e6987ddc963595edb8eeac53ec6b44a8b7
gary909 <garywhite61@hotmail.com>
2023-04-20 12:54:40 +0200

    code

---
+++ app.js
@@ -0,0 +1,11 @@
+function isOpposite(s1,s2){
+  //coding here...
+  
+}
+
+console.log(isOpposite("ab","AB")); // true
+console.log(isOpposite("aB","Ab")); // true
+console.log(isOpposite("aBcd","AbCD")); // true
+console.log(isOpposite("aBcde","AbCD")); // false
+console.log(isOpposite("AB","Ab")); // false
+console.log(isOpposite("","")); // falses
